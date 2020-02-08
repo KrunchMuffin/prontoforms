f6966b5dd75328bc3906322d79f116d8d16f7a42
Derek <db@bowesmail.com>
2020-02-08 02:51:00 -0500

    Updates

---
+++ precache-manifest.b52694d8f2ea7098e0cac2141f65ad85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ae07f08c8f4dafff6adc122e833f17b",
+    "url": "/prontoforms/index.html"
+  },
+  {
+    "revision": "30ea07e568f2c9615f0d",
+    "url": "/prontoforms/static/css/main.0e1d35a1.chunk.css"
+  },
+  {
+    "revision": "235203999cc7a1244e9d",
+    "url": "/prontoforms/static/js/2.86d0ac4d.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/prontoforms/static/js/2.86d0ac4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30ea07e568f2c9615f0d",
+    "url": "/prontoforms/static/js/main.25561424.chunk.js"
+  },
+  {
+    "revision": "23cf96a8c434cd295117",
+    "url": "/prontoforms/static/js/runtime-main.964fe771.js"
+  },
+  {
+    "revision": "d377e76efd51b36e1461003ce4f2913e",
+    "url": "/prontoforms/static/media/loading.d377e76e.gif"
+  }
+]);
